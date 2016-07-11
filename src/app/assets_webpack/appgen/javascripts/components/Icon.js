d3fd23f5fe8247f3e4e5523986b4b7d6426a1632
Tobiáš Potoček <tobiaspotocek@gmail.com>
2016-07-11 23:23:19 +0200

    Attempt to speed up icon loading

--- src/app/assets_webpack/appgen/javascripts/components/Icon.js
+++ src/app/assets_webpack/appgen/javascripts/components/Icon.js
@@ -1,32 +1,18 @@
 import React from 'react'
-import * as Icons from 'material-ui/svg-icons';
+import iconNames from '../icons'
 
-/**
- * Transform icon name from the CamelCase format into the format with underscores. Also the
- * Material UI library prefixes the icon names with categories. Those prefixes are also removed.
- *
- * Example: "ImagePhotoSizeSelectSmall" transform into "photo_size_select_small"
- *
- * The "decapitalized" names correspond to how the icons are named when used with the web font.
- * (Which was the original way icons were used in this app. This is a wrapper providing compatibility)
- */
-const decapitalize = str => {
-  const segments = str.split(/(?=[A-Z])/)
-  segments.shift(); // Remove first part which is the icon category
-  return segments.map(segment => segment.toLowerCase()).join('_')
-};
-
-// Store the SVG icons under their decapitalized names.
 const icons = {};
-Object.keys(Icons).forEach(iconName => {
-  icons[decapitalize(iconName)] = Icons[iconName];
+iconNames.forEach(iconName => {
+  const component = require('material-ui/svg-icons/' + iconName.replace(/_/g, '-')).default;
+  icons[iconName] = component;
+  icons[iconName.replace(/[^\/]+\//, '')] = component;
 });
 
 const Icon = props => {
   const IconComponent = icons[props.icon];
 
   if (!IconComponent) {
-    console.warn(`The icon "${props.icon}" does not exist!`);
+    console.warn(`The icon "${props.icon}" does not exist! Have you added this icon to the icons.js file?`);
     return <div {...props}>Missing icon!</div>
   }
 
---
+++ src/app/assets_webpack/appgen/javascripts/icons.js
@@ -0,0 +1,23 @@
+
+export default [
+  'action/code',
+  'action/dashboard',
+  'action/delete',
+  'action/donut_large',
+  'action/done',
+  'action/done_all',
+  'action/explore',
+  'action/find_in_page',
+  'action/settings',
+  'alert/warning',
+  'communication/screen_share',
+  'content/add',
+  'hardware/device_hub',
+  'hardware/keyboard_arrow_down',
+  'maps/map',
+  'navigation/expand_more',
+  'navigation/expand_less',
+  'navigation/more_vert',
+  'social/person',
+  'social/person_outline'
+];
